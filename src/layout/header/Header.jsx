751f9b3affb1796ef2429c63993664e897212f7a
Koli Oyama <104010042+KoliOyama@users.noreply.github.com>
2024-05-09 17:36:17 +0100

    updated code in sass partials and home page

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,43 +1,46 @@
-import React, {useState} from 'react';
-import { HiUser } from "react-icons/hi";
-import { HiMenuAlt4 } from "react-icons/hi";
-import { HiOutlineX } from "react-icons/hi";
-import { Link } from "react-router-dom";
-import "./header.scss";
-import Logo from "../../assets/Scratchy-Nib-logo.svg";
-import VisitorCounter from "./components/visitor-counter/VisitorCounter";
+import React, { useState } from 'react';
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { HiOutlineX } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
+import './header.scss';
+import VisitorCounter from './components/visitor-counter/VisitorCounter';
 
 const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(true);
+
+  function toggleMenu() {
+    setMenuOpen((prevMenuOpen) => !prevMenuOpen);
+  }
+
   return (
     <header className="header">
-      <div className="header__content">
-        <div className="header__menu-toggle">
-          <HiMenuAlt4 />
+      <div className="header__content container">
+        <div className="header-toggle" onClick={toggleMenu}>
+          {menuOpen ? <HiOutlineX /> : <HiMenuAlt4 />}
         </div>
-        <nav className={"header__nav-container" + (menuOpen ? "isMenu" : "")}>
-          <ul className="header__nav-links">
-            <li className="header__nav-link">
+        <nav className={`nav-container ${menuOpen ? 'isMenu' : ''}`}>
+          <ul className="nav-links">
+            <li className="nav-link">
               <Link to="/">Home</Link>
             </li>
-            <li className="header__nav-link">
+            <li className="nav-link">
               <Link to="/learn">Learn</Link>
             </li>
-            <li className="header__nav-link">
+            <li className="nav-link">
               <Link to="/about">About</Link>
             </li>
-            <li className="header__nav-link">
+            <li className="nav-link">
               <Link to="/gallery">Gallery</Link>
             </li>
-            <li className="header__nav-link">
+            <li className="nav-link">
               <Link to="/contact">Contact</Link>
             </li>
           </ul>
         </nav>
 
-        <div className="header__logo-container">
-          <Link to="/">
-            <img src={Logo} alt="Scratchy nib logo" />
+        <div className="logo-container">
+          <Link to="/" className="header-logo">
+            Scratchy Nib
           </Link>
         </div>
 
